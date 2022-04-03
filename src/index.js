74a7abbfe492b4caa493eccb090631e2c74e7994
Sajadmhy <sajad.mahyaei@gmail.com>
2022-04-03 15:48:49 -0400

    tranfer code to inbox module

--- src/index.js
+++ src/index.js
@@ -1,61 +1,7 @@
+import {closeItem, checkItem, addNew } from './modules/inbox.js';
 
-
-const main = document.getElementById("main");
-const newTask = document.getElementById("new-task");
-const inbox = document.getElementById("inbox");
-const addPro = document.getElementById("add-pro");
-const addB = document.getElementById("add");
-var closebtns = document.getElementsByClassName("close");
-var items = document.getElementsByClassName("item");
-
-function closeItem() { for (let i=0; i < closebtns.length; i++) {
-    closebtns[i].addEventListener("click", function() {
-        this.parentElement.style.display = 'none';
-    });
-}};
-
-function checkItem() {    
-    for (let j=0; j < items.length; j++) {
-    items[j].addEventListener("click", function() {
-        this.classList.toggle("checked");
-        });
-}};
 
 closeItem();
 checkItem();
+addNew();
 
-var input = document.getElementById("task");
-
-function newItem() {
-    if (input.value === "") {
-        alert("You must write something!");
-    } else {
-        var div = document.createElement("div");
-        var li = document.createElement("li");
-        var span = document.createElement("span");
-        span.classList = "close";
-        var itemT = document.createTextNode(input.value);
-        li.appendChild(itemT);
-
-        var icon = document.createTextNode("\u00D7");
-        span.appendChild(icon);
-        div.appendChild(li);
-        div.appendChild(span);
-        div.classList = "item";
-        main.appendChild(div);
-    };
-
-    // main.innerHTML += `<div class="item"><li>${input.value}</li><span class="close">&times;</span></div>`;
-
-};
-
-addB.addEventListener("click", function() {
-    newItem();
-    // checkItem();    
-    closeItem();
-
-    items[items.length-1].addEventListener("click", function() {
-        this.classList.toggle("checked");
-        });
-
-    });
---
+++ src/modules/inbox.js
@@ -0,0 +1,63 @@
+
+
+const main = document.getElementById("main");
+const newTask = document.getElementById("new-task");
+const inbox = document.getElementById("inbox");
+const addPro = document.getElementById("add-pro");
+const addB = document.getElementById("add");
+var closebtns = document.getElementsByClassName("close");
+var items = document.getElementsByClassName("item");
+
+function closeItem() { for (let i=0; i < closebtns.length; i++) {
+    closebtns[i].addEventListener("click", function() {
+        this.parentElement.style.display = 'none';
+    });
+}};
+
+function checkItem() {    
+    for (let j=0; j < items.length; j++) {
+    items[j].addEventListener("click", function() {
+        this.classList.toggle("checked");
+        });
+}};
+
+
+var input = document.getElementById("task");
+
+function newItem() {
+    if (input.value === "") {
+        alert("You must write something!");
+    } else {
+        var div = document.createElement("div");
+        var li = document.createElement("li");
+        var span = document.createElement("span");
+        span.classList = "close";
+        var itemT = document.createTextNode(input.value);
+        li.appendChild(itemT);
+
+        var icon = document.createTextNode("\u00D7");
+        span.appendChild(icon);
+        div.appendChild(li);
+        div.appendChild(span);
+        div.classList = "item";
+        main.appendChild(div);
+    };
+
+    // main.innerHTML += `<div class="item"><li>${input.value}</li><span class="close">&times;</span></div>`;
+
+};
+
+function addNew() {
+    addB.addEventListener("click", function() {
+    newItem();
+    // checkItem();    
+    closeItem();
+
+    items[items.length-1].addEventListener("click", function() {
+        this.classList.toggle("checked");
+        });
+
+    })};
+
+
+    export {closeItem, checkItem, addNew };
