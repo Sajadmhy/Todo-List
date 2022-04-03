eb69fc4ad2ebb10db68bec8071abd635127388d0
Sajadmhy <sajad.mahyaei@gmail.com>
2022-04-03 14:30:47 -0400

    fixa bug about checking items

--- src/index.js
+++ src/index.js
@@ -5,21 +5,19 @@ const newTask = document.getElementById("new-task");
 const inbox = document.getElementById("inbox");
 const addPro = document.getElementById("add-pro");
 const addB = document.getElementById("add");
-
 var closebtns = document.getElementsByClassName("close");
 var items = document.getElementsByClassName("item");
-var i;
-var j;
 
-function closeItem() { for (i=0 ; i < closebtns.length; i++) {
+function closeItem() { for (let i=0; i < closebtns.length; i++) {
     closebtns[i].addEventListener("click", function() {
         this.parentElement.style.display = 'none';
     });
 }};
 
-function checkItem() {for (j=0; j< items.length; j++) {
+function checkItem() {    
+    for (let j=0; j < items.length; j++) {
     items[j].addEventListener("click", function() {
-        this.classList.toggle('checked');
+        this.classList.toggle("checked");
         });
 }};
 
@@ -41,19 +39,23 @@ function newItem() {
 
         var icon = document.createTextNode("\u00D7");
         span.appendChild(icon);
-        li.appendChild(span);
         div.appendChild(li);
+        div.appendChild(span);
         div.classList = "item";
         main.appendChild(div);
-
-
-
+    };
 
     // main.innerHTML += `<div class="item"><li>${input.value}</li><span class="close">&times;</span></div>`;
-        closeItem();
-        checkItem();
-}};
+
+};
 
 addB.addEventListener("click", function() {
     newItem();
+    // checkItem();    
+    closeItem();
+
+    items[items.length-1].addEventListener("click", function() {
+        this.classList.toggle("checked");
+        });
+
     });
