9521efbd3539133ae407055568f4dc90327e8b4a
Sajadmhy <sajad.mahyaei@gmail.com>
2022-04-04 09:41:33 -0400

    add another solution with basic functionality and localStorage

--- src/modules/inbox.js
+++ src/modules/inbox.js
@@ -1,13 +1,10 @@
 
 
 const main = document.getElementById("main");
-// const newTask = document.getElementById("new-task");
-// const inbox = document.getElementById("inbox");
-// const addPro = document.getElementById("add-pro");
 const addB = document.getElementById("add");
 var closebtns = document.getElementsByClassName("close");
-var items = document.getElementsByClassName("item");
-
+// var items = document.getElementsByClassName("item");
+var items = ['hit the gym' , 'pay bills'];
 function closeItem() { for (let i=0; i < closebtns.length; i++) {
     closebtns[i].addEventListener("click", function() {
         this.parentElement.style.display = 'none';
@@ -39,11 +36,52 @@ function newItem() {
     if (input.value === "") {
         alert("You must write something!");
     } else {
+        // var div = document.createElement("div");
+        // var li = document.createElement("li");
+        // var span = document.createElement("span");
+        // span.classList = "close";
+        // var itemT = document.createTextNode(input.value);
+        // li.appendChild(itemT);
+
+        // var icon = document.createTextNode("\u00D7");
+        // span.appendChild(icon);
+        // div.appendChild(li);
+        // div.appendChild(span);
+        // div.classList = "item";
+        // main.appendChild(div);
+        items.push(input.value);
+        populateItems();
+    };
+
+};
+
+function addNew() {
+    addB.addEventListener("click", function() {
+    clearDiv(); 
+    newItem();
+    closeItem();
+    });
+};
+
+    // items[items.length-1].addEventListener("click", function() {
+    //     this.classList.toggle("checked");
+    //     });
+
+    // })};
+
+function clearDiv() {
+    document.getElementById("items").innerHTML="";
+}
+    
+
+
+function populateItems() {
+    for (let k=0; k< items.length; k++) {
         var div = document.createElement("div");
         var li = document.createElement("li");
         var span = document.createElement("span");
         span.classList = "close";
-        var itemT = document.createTextNode(input.value);
+        var itemT = document.createTextNode(items[k]);
         li.appendChild(itemT);
 
         var icon = document.createTextNode("\u00D7");
@@ -51,31 +89,10 @@ function newItem() {
         div.appendChild(li);
         div.appendChild(span);
         div.classList = "item";
-        // main.appendChild(div);
-        items.push(div);
-    };
-
-    // main.innerHTML += `<div class="item"><li>${input.value}</li><span class="close">&times;</span></div>`;
-
+        main.appendChild(div);  
+    }
 };
 
-function addNew() {
-    addB.addEventListener("click", function() {
-    newItem();
-    closeItem();
-
-    items[items.length-1].addEventListener("click", function() {
-        this.classList.toggle("checked");
-        });
-
-    })};
-    
-    // function addToLocalStorage(todos) {
-    //     localStorage.setItem('todos', JSON.stringify(todos));
-
-    // }
 
 
-
-
-    export {closeItem, checkItem, addNew };
+    export {closeItem, checkItem, addNew, populateItems };
